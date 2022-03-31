9bbe3cbbaa110288fcd11231ef4134f8fde99e2e
lucassodrealves <lucassodrealves4@gmail.com>
2022-03-31 10:45:02 -0300

    adicionando a notação Repository

--- Documents/workspace-spring-tool-suite-4-4.13.1.RELEASE/gamesNovo/src/main/java/com/generation/gamesNovo/repository/ClasseRepository.java
+++ Documents/workspace-spring-tool-suite-4-4.13.1.RELEASE/gamesNovo/src/main/java/com/generation/gamesNovo/repository/ClasseRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.gamesNovo.model.Classe;
 
+@Repository
 public interface ClasseRepository extends JpaRepository<Classe, Long>{
 	public List<Classe> findAllByNomeContainingIgnoreCase(String nome);
 	
